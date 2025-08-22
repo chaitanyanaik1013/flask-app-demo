1f4ab136d2267b3bb856cb9883056d214ff1c9af
Chaitanya Naik <naikchaitanyaabc@gmail.com>
2025-08-22 14:20:09 +0000

    Second commit

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from forms import LoginForm
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h1>Hello World</h1>"
+def index():
+    tasks = ["Day 2", "Learning Jinja", "Building my portfolio"]
+    return render_template('index.html', task_list = tasks)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User {username}'
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {subpath}'
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form_fields = form)
 
 
 if __name__ == "__main__":
